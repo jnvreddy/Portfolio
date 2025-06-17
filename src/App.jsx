a7335956b640e0a91fc2c520ad484168a9b8bf35
jnvreddy <jnvreddyofficial@gmail.com>
2025-06-17 20:07:04 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-gray-50 min-h-screen">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black shadow-md fixed w-full h-40 z-10">
+      
+    </footer>
+  );
+};
+
+export default Footer;
