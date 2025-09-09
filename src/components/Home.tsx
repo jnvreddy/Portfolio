dbe65a21e636e9aa627a8ee27d3a525fa551cc46
jnvreddy <jnvreddyofficial@gmail.com>
2025-09-09 20:59:34 +0530

    content removed from home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,13 +5,6 @@ const Home = () => {
     <div className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100 transition-colors duration-300">
       <Header />
 
-      <section
-        id="home"
-        className="flex flex-col h-[100vh] items-center justify-center text-center py-24 px-6 transition-colors duration-300"
-      >
-        
-      </section>
-
 
     </div>
   );
